418af81ec142b0af0a4e1668cc52d498809009a8
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-02-21 11:53:09 +0600

    post user info in DB

--- src/Components/GoogleLogin/GoogleLogin.jsx
+++ src/Components/GoogleLogin/GoogleLogin.jsx
@@ -3,14 +3,25 @@ import { GoogleAuthProvider } from "firebase/auth";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import axios from "axios";
 
 const GoogleLogin = () => {
-  const { googleSignIn,successfulToast } = useContext(AuthContext);
+  const { googleSignIn, successfulToast } = useContext(AuthContext);
   const googleProvider = new GoogleAuthProvider();
   const navigate = useNavigate();
   const handleGoogleSignIn = async () => {
     try {
-      const data = await googleSignIn(googleProvider);
+      const { user } = await googleSignIn(googleProvider);
+      const userDetails = {
+        email: user?.email,
+        name: user?.displayName,
+        userId: user?.uid,
+      };
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API}/users`,
+        userDetails
+      );
+      console.log(data);
       navigate("/");
       successfulToast("Successfully login with Google");
     } catch (err) {}
